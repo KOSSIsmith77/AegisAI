64f9fd032b3a3f234c10c34e08ce4032ff6038b7
KOSSIsmith77 <attissosmith2@gmail.com>
2026-07-09 22:53:26 +0000

    Add Footer component with copyright information

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-center text-gray-400 py-8">
+      © 2026 AegisAI - Tous droits réservés.
+    </footer>
+  );
+}
