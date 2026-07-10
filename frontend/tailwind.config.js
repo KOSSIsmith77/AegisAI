70093b8859e964ff15e6862db152b9c448df26a2
KOSSIsmith77 <attissosmith2@gmail.com>
2026-07-10 18:45:24 +0000

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    content:[
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}"
+    ],
+
+    theme:{
+        extend:{
+            colors:{
+                cyber:{
+                    dark:"#050816",
+                    blue:"#00e5ff",
+                    purple:"#8b5cf6"
+                }
+            }
+        }
+    },
+
+    plugins:[]
+};
