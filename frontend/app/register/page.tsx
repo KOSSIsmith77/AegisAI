023fa06d1998fde5bf7a947e1ae52b716b1f9571
KOSSIsmith77 <attissosmith2@gmail.com>
2026-07-10 22:43:51 +0000

    Create page.tsx

---
+++ frontend/app/register/page.tsx
@@ -0,0 +1,15 @@
+import RegisterForm from "@/components/RegisterForm";
+
+export default function Register(){
+
+    return(
+
+        <main className="min-h-screen flex items-center justify-center">
+
+            <RegisterForm/>
+
+        </main>
+
+    )
+
+}
